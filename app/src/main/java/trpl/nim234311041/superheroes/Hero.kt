a4ef4737366b5a5f4aed7c7e5e326cfe969ee44d
Hanan labib <hananlabib99@gmail.com>
2024-12-05 20:41:55 +0700

    NIM 234311041

---
+++ app/src/main/java/trpl/nim234311041/superheroes/Hero.kt
@@ -0,0 +1,10 @@
+package trpl.nim234311041.superheroes
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
